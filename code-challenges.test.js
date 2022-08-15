b02ad3f2e9b0612e39c294b936d2c88d039d94da
roeckerc <roeckerc@gmail.com>
2022-08-15 08:52:39 -0700

    turning in my assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,21 +12,34 @@
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
+
+
+
+//   const areYouHungry = (string) => {
+//     if (string === "yes") {
+//       return "eat food";
+//     } else if (string === "no") {
+//       return "keep working";
+//     }
+//   };
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
+// // Expected output: [72, 81, 90, 99, 108]
 
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
 
 
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
+
+
 // a) Create a test with expect statements for each of the variables provided.
+
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -35,15 +48,44 @@ const object2 =  { number: 0 }
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
+// create a checker  with a funciton called "demarcateThrice", it checks to see if the value of a key of an object is devisable by 3.
+// later when i write the function i can check if its devisible by 3 by using % 3 and looking to see if it strictly equals zero. 
 
+
+describe("demarcateThrice", () => {
+    it("checks to see if the value of a key of an object is devisable by 3", () => {
+      expect(demarcateThrice(object1.number)).toEqual(`15 is divisible by three`);
+      expect(demarcateThrice(object2.number)).toEqual(`0 is divisible by three`);
+      expect(demarcateThrice(object3.number)).toEqual(`-7 is not divisible by three`);
+    });
+  });
+//returned 1 fail.
 // b) Create the function that makes the test pass.
+// create a funciton called "demarcateThrice", it checks to see if the value of a key of an object is devisable by 3.
+// i can check if its devisible by 3 by using % 3 and looking to see if it strictly equals zero. 
+// i can then have it return the statement, starting with the number that it used, then say if it is of is not divisible by three.
+
+
+const demarcateThrice = (number) => {
+    if (number %3 === 0) {
+        return `${number} is divisible by three`
+    } else {
+        return `${number} is not divisible by three`
+    }
+}
 
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
-
+describe("firstLetterFertalizer", () => {
+    it("takes the first letter and capitalizes it", () => {
+      expect(firstLetterFertalizer(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]);
+      expect(firstLetterFertalizer(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]);
+    });
+  });
+//returned 1 fail
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
@@ -51,12 +93,35 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deducti
 
 
 // b) Create the function that makes the test pass.
-
+    // declare a function that takes in an array
+    // create a local variable that maps through the array
+    // with each value access the first index and capitalize the letter then concatenate the remainder of the word
+    // return the local variable joined into a string
+    const firstLetterFertalizer = (array) => {
+        
+      let eachItem = array.map((value) => {
+        
+          
+          return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+      });
+   
+       return eachItem;
+      }
+      
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("proplemFromhell", () => {
+  it("checks to see where the first vowel is and logs that index ", () => {
+    expect(proplemFromhell(vowelTester1)).toEqual(1);
+    expect(proplemFromhell(vowelTester2)).toEqual(0);
+    expect(proplemFromhell(vowelTester3)).toEqual(2);
+  });
+});
+
+//returned 1 fail
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -64,6 +129,36 @@ const vowelTester2 = "academy"
 // Expected output: 0
 const vowelTester3 = "challenges"
 // Expected output: 2
-
+//pseudocode: make a function
+// look for vowels and log the index of them if non log -1.
+// get rid of all the index less than 0
+// organize smallest to largest putting the smallest number in the 0 index of new array.
+//get the 0 index which is the first location of the vowel.
+// i spent like 4 hours on this and it was way easier than i made it cause i was stupid. see practice.js to see the pain. i deleted so much and was still left with all that dumb stuff hahahaha. wow this one rocked me. I really wish i did the concepts and interview questions first, it would have made this wayyyyy easier. 
 
 // b) Create the function that makes the test pass.
+const proplemFromhell = (value) =>{
+  const aLittleBitOfWiskeyAtBfastNeverHurtAnyone = (value) =>
+  value.indexOf("a");
+  const exceptMyMarriageIGuess = (value) => value.indexOf("e");
+  const incandecentLightbulbsTasteBad = (value) => value.indexOf("i");
+  const officeOfTheSecretaryAndMistressToClinton = (value) => value.indexOf("o");
+  const unacceptableChoicesForVariableNamesMr = (value) => value.indexOf("u");
+
+  var joinedAtTheHip =        ([aLittleBitOfWiskeyAtBfastNeverHurtAnyone(value),exceptMyMarriageIGuess(value), incandecentLightbulbsTasteBad(value), officeOfTheSecretaryAndMistressToClinton(value), unacceptableChoicesForVariableNamesMr(value)])
+
+
+  const littleBumpAndGrindNeverHurtAnyone = (array) => {
+      let newArr = [];
+      for (let i = 0; i < array.length; i++) {
+          if (array[i] >= 0){
+              newArr.push(joinedAtTheHip[i]);
+           }
+       }
+      return newArr
+  };
+
+  var oneMoreStep = (littleBumpAndGrindNeverHurtAnyone(joinedAtTheHip)).sort((a, b) => a-b);
+  var hereIsTheStupidAnserOMGThatTookSoLong =  oneMoreStep[0];
+  return hereIsTheStupidAnserOMGThatTookSoLong
+}
--- coding-concepts.js
+++ coding-concepts.js
@@ -2,72 +2,95 @@
 
 // Examine the following examples.
 
-// First, predict the outcome based on your understanding of the code.
-// Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
-
-// --------------------1) What will this log?
-
-const cohort = "Echo 2022"
-// console.log(cohort.split(""))
-
-// a) Your answer:
-// b) Verify and explain:
+// a describe method that lists the name of the function OR naming of the particular test.
 
 
-// --------------------2) What will this log?
+// // First, predict the outcome based on your understanding of the code.
+// // Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
 
-const greeter = (name) => {
-  `Hello, ${name}!`
-}
-// console.log(greeter("LEARN Student"))
+// // --------------------1) What will this log?
 
-// a) Your answer:
-// b) Verify and explain:
+// const cohort = "Echo 2022"
+// // console.log(cohort.split(""))
 
-
-// --------------------3) What will this log?
-
-const multipliedByTwo = [4, 5, 6, 7, 8].map(number => number * 2)
-// console.log(multipliedByTwo)
-
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: This will log the array: 
+//      ['E', 'c', 'h', 'o', ' ', '2', '0', '2', '2']
+// // b) Verify and explain:
+// I was correct. .Split is an Accessor. Accessor methods dont actually change the array. They will output something, but if you were to just console.log(cohort), it would still be = "Echo 2022". If it wasnt going straight into a concole log, you would have to save it as a varible ex: var imSoTired = cohort.split("").
+// I got super lucky on guessing how it would split it up. The place you have the "'s when using .split will change the answer ex:
+// if it had been console.log(cohort.split(" ")) the reuslt would have benn ["Echo", "2022"]
 
 
-// --------------------4) What will this log?
+// // --------------------2) What will this log?
 
-const onlyOdds = [11, 12, 13, 14, 15].filter(number => number % 2 !== 0)
-// console.log(onlyOdds)
+// const greeter = (name) => {
+//   `Hello, ${name}!`
+// }
+// // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
-
-
-// --------------------5) What will this log?
-
-const myCodingSkills = {
-  languages: ["JavaScript", "Ruby"],
-  frameworks: ["React", "Ruby on Rails"],
-  databases: "PostgreSQL",
-  versionControl: "GitHub"
-}
-// console.log(myCodingSkills.languages[0])
-
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: This will log "undefined". Every function must have a return. 
+// // b) Verify and explain: The parts of a function are
+// 1 variable declaration
+// Variable name
+// Single equal sign
+// Parentheses
+// Arrow
+// Curly Braces
+// Return 
+// without parts of this, it would not be a function. There is a thing called syntactical sugar that you could use, but the concept is still the same. A function is a recipe, but you have to have something that comes out at the end or else the recipe is pointless. The return is necessary.
 
 
-// --------------------STRETCH: What will this log?
+// // --------------------3) What will this log?
 
-class Learn {
-  constructor(name){
-    this.student = name,
-    this.cohort = "Echo",
-    this.year = 2022
-  }
-}
-const learnStudent = new Learn("George")
-// console.log(learnStudent)
+// const multipliedByTwo = [4, 5, 6, 7, 8].map(number => number * 2)
+// // console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: [8, 10, 12, 14, 16].  
+// // b) Verify and explain: line 45 is declaring a variable named multipliedByTwo. multipliedByTwo = and array that is being mapped over with a function. .map is a higher order function that goes over each part of an array and does or check something to each item at an index.
+// In this case it is mapping over the array and multiplying each number by 2 and returning a new array that is the same length.  
+
+// // --------------------4) What will this log?
+
+// const onlyOdds = [11, 12, 13, 14, 15].filter(number => number % 2 !== 0)
+// // console.log(onlyOdds)
+
+// // a) Your answer:[11, 13, 15]
+// // b) Verify and explain: line 54 is declaring a variable name onlyOdds. onlyOdds = an array that is being filtered over with a function. .filter is a higher order function, alot like .map, but it goes over each part of an array and does or checks something to each item at an index, ,but it does not have to return an array of the same length. it will only return the things that were true with the condition set. 
+// in this case the condition that was set was that whatever the number was if it was divisible by 2 and had a remainder of some kind, then that meant it was an odd number so it returned that number. Only 3 of the numbers were odd so it returned an array with 3 items.  
+
+
+// // --------------------5) What will this log?
+
+// const myCodingSkills = {
+//   languages: ["JavaScript", "Ruby"],
+//   frameworks: ["React", "Ruby on Rails"],
+//   databases: "PostgreSQL",
+//   versionControl: "GitHub"
+// }
+// // console.log(myCodingSkills.languages[0])
+
+// // a) Your answer: JavaScript
+// // b) Verify and explain: i was right. What we have here is an object. the object is myCoding Skills. Objects have keys and value pairs. keys and value are the 2 parts to an object. In this example, the keys are languages, frameworks, databases, and versionControl. they are all a datatype of symbol. the value of each of the keys is to the right of them.
+//there are 2 values for languages. 2 for framweorks, 1 for databases, and 1 for VersionControl. The values for languages or organized in an array, so on line 70 the log is asking for whatever information is located at: the object of "myCodingSkills", with the key of "languages", inside of an array at the index of "0". all we do is find our way down the ladder and we end up at "JavaScript"
+
+
+// // --------------------STRETCH: What will this log?
+
+// class Learn {
+//   constructor(name){
+//     this.student = name,
+//     this.cohort = "Echo",
+//     this.year = 2022
+//   }
+// }
+// const learnStudent = new Learn("George")
+// // console.log(learnStudent)
+
+// // a) Your answer: Learn { student: 'George', cohort: 'Echo', year: 2022 }
+// // b) Verify and explain: i was correct. A class is a function, but it also has data in it in them. they are the cad drawing blue prints of an object. 
+//  on line 86 an obect (learnStudent) was made. 
+// LearnStudent:
+// key:student  value: name : aka George
+// key: cohort  value: "Echo"
+// key: year value: 2022.
+// online 87 we are logging what learnStudent is. learn student is in the class of "Learn" followed by {} containing the key value pairs 
